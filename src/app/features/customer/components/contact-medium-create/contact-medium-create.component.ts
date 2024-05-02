f08dc0787d22452cd6116fb443686d5a20d14adb
enesokur <179an67@gmail.com>
2024-05-02 17:21:07 +0300

    refactor

--- src/app/features/customer/components/contact-medium-create/contact-medium-create.component.ts
+++ src/app/features/customer/components/contact-medium-create/contact-medium-create.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { InputComponent } from '../../../../shared/components/input/input.component';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact-medium-create',
   standalone: true,
-  imports: [InputComponent, ButtonComponent],
+  imports: [InputComponent, ButtonComponent,RouterLink],
   templateUrl: './contact-medium-create.component.html',
   styleUrl: './contact-medium-create.component.scss',
 })
--- src/app/features/customer/components/customer-address-create/customer-address-create.component.ts
+++ src/app/features/customer/components/customer-address-create/customer-address-create.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { InputComponent } from '../../../../shared/components/input/input.component';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-customer-address-create',
   standalone: true,
-  imports: [InputComponent],
+  imports: [InputComponent,ButtonComponent,RouterLink],
   templateUrl: './customer-address-create.component.html',
   styleUrl: './customer-address-create.component.scss'
 })
