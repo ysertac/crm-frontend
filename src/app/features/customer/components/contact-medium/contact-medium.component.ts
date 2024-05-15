8451d1d8c1ea6398a8926de60206c4a64d353181
Melsa Sağın <melsa.sagin@etiya.com>
2024-05-15 17:00:04 +0300

    add get contact-medium

--- src/app/features/customer/components/contact-medium/contact-medium.component.ts
+++ src/app/features/customer/components/contact-medium/contact-medium.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { ContactMediumApiService } from '../../services/contact-medium-api.service';
 import { PostContactMediumRequest } from '../../models/contact-medium/post-contact-medium-request';
 import { Store } from '@ngrx/store';
-import { GetContactMediumResponse } from '../../models/contact-medium/get-contact-medium-response';
 import { setContactMedium } from '../../../../shared/store/contactMedium/contact-medium.action';
+import { GetContactMediumByCustomerIdResponse } from '../../models/contact-medium/get-contact-medium-by-customer-id-response';
 
 @Component({
   selector: 'app-contact-medium',
@@ -16,34 +16,35 @@ import { setContactMedium } from '../../../../shared/store/contactMedium/contact
 })
 export class ContactMediumComponent implements OnInit {
   customerId: string;
-  contactMediumDetail: GetContactMediumResponse;
+  contactMediumDetail: GetContactMediumByCustomerIdResponse;
 
   constructor(
     private contactMediumApiService: ContactMediumApiService,
+    private router: Router,
     private store: Store<{ contactMedium: PostContactMediumRequest }>
   ) {}
 
   ngOnInit(): void {
-    this.getById();
+    const customerId = this.router.url.split('/')[3];
+    this.getById(customerId);
   }
 
-  getById() {
-    this.contactMediumApiService
-      .getById('b1bea412-93a7-48ef-866d-e1963197d303')
-      .subscribe({
-        next: (response) => {
-          this.contactMediumDetail = response;
-          this.createContactMedium(response);
-        },
-        error: (error) => {
-          console.error('Error:', error);
-        },
-      });
+  getById(customerId: string) {
+    this.contactMediumApiService.getById(customerId).subscribe({
+      next: (response) => {
+        this.contactMediumDetail = response;
+        this.createContactMedium(response);
+        console.log(response);
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      },
+    });
   }
 
-  createContactMedium(response: GetContactMediumResponse) {
+  createContactMedium(response: GetContactMediumByCustomerIdResponse) {
     const contactMedium: PostContactMediumRequest = {
-      customerId: '',
+      customerId: response.customerId,
       email: response.email,
       homePhone: response.homePhone,
       mobilePhone: response.mobilePhone,
---
+++ src/app/features/customer/models/contact-medium/get-contact-medium-by-customer-id-response.ts
@@ -0,0 +1,8 @@
+export interface GetContactMediumByCustomerIdResponse {
+  id: string;
+  customerId: string;
+  email: string;
+  homePhone: string;
+  mobilePhone: string;
+  fax: string;
+}
